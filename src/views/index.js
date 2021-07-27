ad7770227894d6fcdf50a7e83556dff57708c3e4
paolataboada <taboadapaola3@gmail.com>
2021-07-27 18:13:48 -0500

    agregando css a la interfaz

--- src/views/index.js
+++ src/views/index.js
@@ -1,13 +1,13 @@
 import registro from './registro.js';
 import inicio from './inicio.js';
-import perfil from './perfil.js';
-import perfilOtros from './perfilOtros.js';
+/* import perfil from './perfil.js';
+import perfilOtros from './perfilOtros.js'; */
 
 const componentes = {
   Registro: registro,
   Inicio: inicio,
-  Perfil: perfil,
-  PerfilOtros: perfilOtros,
+  /* Perfil: perfil,
+  PerfilOtros: perfilOtros, */
 };
 
-export { componentes }
+export { componentes };

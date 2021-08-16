1523bcfb96ced2e0aec05b37db117706b862f7eb
DafneAquino <dafneaquinom@gmail.com>
2021-08-15 21:24:03 -0500

    vacio

--- src/views/index.js
+++ src/views/index.js
@@ -1,10 +1,1 @@
-import logIn from './viewLogin.js';
-import home from './viewHome.js';
-import signUp from './viewSignup.js';
 
-const componentes = {
-  LogIn: logIn,
-  Home: home,
-  Signup: signUp,
-};
-export { componentes };
